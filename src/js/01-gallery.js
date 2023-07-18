fd1850fe49738284838333b444cef0c2541bf894
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-07-18 21:40:13 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { galleryItems } from './gallery-items';
 
