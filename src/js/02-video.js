bcaccdd58b52d85c18b940c33dbf87073252e1c9
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-07-19 16:01:53 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', function (data) {
+  duration: 61.857;
+  percent: 0.049;
+  seconds: 3.034;
+  console.log(data);
+  const currentTime = data.seconds;
+  console.log(currentTime);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+});
